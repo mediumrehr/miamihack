fbd3d234e609b76b3bc7cd284eaf51fc3a250f74
Brian Gerstle <bgerstle@spotify.com>
2014-03-22 14:15:54 -0400

    test files

---
+++ Tests/TRPArtistSearchTests.m
@@ -0,0 +1,15 @@
+//
+//  TRPArtistSearchTests.m
+//  Tripster
+//
+//  Created by Brian Gerstle on 3/22/14.
+//  Copyright (c) 2014 UMiami. All rights reserved.
+//
+
+SpecBegin(ArtistSearch)
+
+describe(@"search", ^{
+
+});
+
+SpecEnd
---
+++ Tests/TRPTripPlaylistTests.m
@@ -0,0 +1,15 @@
+//
+//  TRPTripPlaylistTests.m
+//  Tripster
+//
+//  Created by Brian Gerstle on 3/22/14.
+//  Copyright (c) 2014 UMiami. All rights reserved.
+//
+
+SpecBegin(Playlist)
+
+describe(@"creation", ^{
+
+});
+
+SpecEnd
