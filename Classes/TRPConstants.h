81e007dcfd48dee96bc8d6d4cef55a0fdfa20146
Brian Gerstle <bgerstle@spotify.com>
2014-03-22 11:35:58 -0400

    add API keys

---
+++ Classes/TRPConstants.h
@@ -0,0 +1,25 @@
+//
+//  TRPConstants.h
+//  Tripster
+//
+//  Created by Brian Gerstle on 3/22/14.
+//  Copyright (c) 2014 UMiami. All rights reserved.
+//
+
+#ifndef Tripster_TRPConstants_h
+#define Tripster_TRPConstants_h
+
+/**
+ Spotify API
+ */
+extern const uint8_t SPOTIFY_APP_KEY[];
+extern const size_t SPOTIFY_APP_KEY_SIZE;
+
+/**
+ EchoNest API
+ */
+extern NSString const* kEchoNestAPIKey;
+extern NSString const* kEchoNestConsumerKey;
+extern NSString const* kEchoNestSharedSecret;
+
+#endif
