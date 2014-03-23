6b2cb87b5853aec00d5b09c059e5b51f1e868204
alayers2 <alayers2@gmail.com>
2014-03-22 23:37:12 -0400

    playbacks a bitch ( but it works) (but only for Shakira’s “She Wolf”… for whatever fucking reason they chose that song)

--- Classes/TRPTripViewController.h
+++ Classes/TRPTripViewController.h
@@ -8,14 +8,32 @@
 
 #import <UIKit/UIKit.h>
 #import "TRPTripModel.h"
-// #import "PlaylistModel.h"
+#import "TRPPlaylistModel.h"
 #import <CocoaLibSpotify/CocoaLibSpotify.h>
 
-@interface TRPTripViewController : UIViewController{
-    SPTrack *_track;
+@interface TRPTripViewController : UIViewController<SPSessionDelegate, SPSessionPlaybackDelegate>{
     IBOutlet UIButton *playButton;
-    SPPlaybackManager *plManager;
+    TRPPlaylistModel *plModel;
+    UILabel *_trackTitle;
+	UILabel *_trackArtist;
+	UIImageView *_coverView;
+	UISlider *_positionSlider;
+	SPPlaybackManager *_playbackManager;
+	SPTrack *_currentTrack;
 }
 - (IBAction)playButtonPressed:(id)sender;
+- (IBAction)setTrackPosition:(id)sender;
+- (IBAction)setVolume:(id)sender;
 
+@property (nonatomic, strong) IBOutlet UILabel *trackTitle;
+@property (nonatomic, strong) IBOutlet UILabel *trackArtist;
+@property (nonatomic, strong) IBOutlet UIImageView *coverView;
+@property (nonatomic, strong) IBOutlet UISlider *positionSlider;
+
+@property (nonatomic, strong) SPTrack *currentTrack;
+@property (nonatomic, strong) SPPlaybackManager *playbackManager;
 @end
+
+
+
+
--- Classes/TRPTripViewController.m
+++ Classes/TRPTripViewController.m
@@ -15,12 +15,26 @@
 
 @implementation TRPTripViewController
 
+@synthesize trackTitle = _trackTitle;
+@synthesize trackArtist = _trackArtist;
+@synthesize coverView = _coverView;
+@synthesize positionSlider = _positionSlider;
+@synthesize playbackManager = _playbackManager;
+@synthesize currentTrack = _currentTrack;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-        plManager = [[SPPlaybackManager alloc] initWithPlaybackSession:[SPSession sharedSession]];
+        self.playbackManager = [[SPPlaybackManager alloc] initWithPlaybackSession:[SPSession sharedSession]];
+        [[SPSession sharedSession] setDelegate:self];
+        
+        [self addObserver:self forKeyPath:@"currentTrack.name" options:0 context:nil];
+        [self addObserver:self forKeyPath:@"currentTrack.artists" options:0 context:nil];
+        [self addObserver:self forKeyPath:@"currentTrack.duration" options:0 context:nil];
+        [self addObserver:self forKeyPath:@"currentTrack.album.cover.image" options:0 context:nil];
+        [self addObserver:self forKeyPath:@"playbackManager.trackPosition" options:0 context:nil];
 
     }
     return self;
@@ -40,25 +54,71 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"currentTrack.name"]) {
+        self.trackTitle.text = self.currentTrack.name;
+	} else if ([keyPath isEqualToString:@"currentTrack.artists"]) {
+		self.trackArtist.text = [[self.currentTrack.artists valueForKey:@"name"] componentsJoinedByString:@","];
+	} else if ([keyPath isEqualToString:@"currentTrack.album.cover.image"]) {
+		self.coverView.image = self.currentTrack.album.cover.image;
+	} else if ([keyPath isEqualToString:@"currentTrack.duration"]) {
+		self.positionSlider.maximumValue = self.currentTrack.duration;
+	} else if ([keyPath isEqualToString:@"playbackManager.trackPosition"]) {
+		// Only update the slider if the user isn't currently dragging it.
+		if (!self.positionSlider.highlighted)
+			self.positionSlider.value = self.playbackManager.trackPosition;
+        
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
 - (IBAction)playButtonPressed:(id)sender {
-    [[SPSession sharedSession] trackForURL:[NSURL URLWithString:@"spotify-US:artist:7qG3b048QCHVRO5Pv1T5lw"] callback:^(SPTrack *track) {
-     if (track != nil) {
-        [SPAsyncLoading waitUntilLoaded:track timeout:kSPAsyncLoadingDefaultTimeout then:^(NSArray *tracks, NSArray *notLoadedTracks) {
-            [plManager playTrack:track callback:^(NSError *error) {
-                if (error) {
-                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Cannot Play Track"
-                                                                             message:[error localizedDescription]
-                                                                            delegate:nil
-                                                                   cancelButtonTitle:@"OK"
-                                                                   otherButtonTitles:nil];
-                             [alert show];
-                         } else {
-                             _track = track;
-                         }
-                         
-                     }];
-                 }];
-             }
-         }];
+	
+	// Invoked by clicking the "Play" button in the UI.
+	NSString *trackurlstring = @"spotify:track:489K1qunRVBm2OpS4XGLNd";
+	if (trackurlstring.length > 0) {
+		
+		NSURL *trackURL = [NSURL URLWithString:trackurlstring];
+		[[SPSession sharedSession] trackForURL:trackURL callback:^(SPTrack *track) {
+			
+			if (track != nil) {
+				
+				[SPAsyncLoading waitUntilLoaded:track timeout:kSPAsyncLoadingDefaultTimeout then:^(NSArray *tracks, NSArray *notLoadedTracks) {
+					[self.playbackManager playTrack:track callback:^(NSError *error) {
+						
+						if (error) {
+							UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Cannot Play Track"
+																			message:[error localizedDescription]
+																		   delegate:nil
+																  cancelButtonTitle:@"OK"
+																  otherButtonTitles:nil];
+							[alert show];
+						} else {
+							self.currentTrack = track;
+						}
+						
+					}];
+				}];
+			}
+		}];
+		
+		return;
+	}
+	
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Cannot Play Track"
+													message:@"Please enter a track URL"
+												   delegate:nil
+										  cancelButtonTitle:@"OK"
+										  otherButtonTitles:nil];
+	[alert show];
+}
+
+- (IBAction)setTrackPosition:(id)sender {
+	[self.playbackManager seekToTrackPosition:self.positionSlider.value];
+}
+
+- (IBAction)setVolume:(id)sender {
+	self.playbackManager.volume = [(UISlider *)sender value];
 }
 @end
