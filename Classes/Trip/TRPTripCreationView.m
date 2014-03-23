f7e1210c6d030716fd562f9578f2d0df85a95175
alayers2 <alayers2@gmail.com>
2014-03-22 23:11:10 -0400

    button is back

--- Classes/Trip/TRPTripCreationView.m
+++ Classes/Trip/TRPTripCreationView.m
@@ -28,6 +28,10 @@
         [self.locationField setDelegate:self];
         self.locationField.borderStyle = UITextBorderStyleRoundedRect;
         [self addSubview:self.locationField];
+        
+        self.createPlaylistButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [self.createPlaylistButton addTarget:self action:@selector(createPlaylist:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:self.createPlaylistButton];
     }
     return self;
 }
