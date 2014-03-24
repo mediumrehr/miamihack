ff1e8d00b628951be7ce04a585517200d9304b98
Brian Gerstle <bgerstle@spotify.com>
2014-03-24 08:19:54 -0400

    rudimentary playlist creation and almost playback

---
+++ Classes/SPUtils/SPPlaylistPlaybackDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SPPlaylistPlaybackDelegate.h
+//  Tripster
+//
+//  Created by Brian Gerstle on 3/24/14.
+//  Copyright (c) 2014 UMiami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPPlaylistPlaybackDelegate : NSObject
+
+@end
---
+++ Classes/SPUtils/SPPlaylistPlaybackDelegate.m
@@ -0,0 +1,13 @@
+//
+//  SPPlaylistPlaybackDelegate.m
+//  Tripster
+//
+//  Created by Brian Gerstle on 3/24/14.
+//  Copyright (c) 2014 UMiami. All rights reserved.
+//
+
+#import "SPPlaylistPlaybackDelegate.h"
+
+@implementation SPPlaylistPlaybackDelegate
+
+@end
